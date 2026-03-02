1c475c05089fd2d3d7933ee1d54da3b84ebebd6a
ejaron16 <ee8691@srmist.edu.in>
2026-03-02 10:59:38 +0530

    Update LVL3_7.java

--- W12-Practice-Problems/LVL3_7.java
+++ W12-Practice-Problems/LVL3_7.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LVL3_7tor {
+public class LVL3_7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter number of chocolates and children: ");
